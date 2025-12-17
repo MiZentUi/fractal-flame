7a69097ad78f1d3e23b41913419e5daf69cc314e
Denis Skrobot <mizentui@gmail.com>
2025-12-17 20:58:57 +0300

    Application and config

--- src/main/java/academy/Application.java
+++ src/main/java/academy/Application.java
@@ -1,40 +1,32 @@
 package academy;
 
+import academy.cli.AppConfig;
+import academy.cli.Options;
+import academy.model.AffineTransformation;
+import academy.model.Image;
+import academy.model.Pixel;
+import academy.utils.Generator;
+import academy.utils.ImageWriter;
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Random;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 @Command(name = "Application Example", version = "Example 1.0", mixinStandardHelpOptions = true)
 public class Application implements Runnable {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-    private static final ObjectReader YAML_READER =
-            new ObjectMapper(new YAMLFactory()).findAndRegisterModules().reader();
+    private static final ObjectReader JSON_READER = new ObjectMapper(new JsonFactory()).setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE).findAndRegisterModules().reader();
 
-    @Option(
-            names = {"-s", "--font-size"},
-            description = "Font size",
-            defaultValue = "12")
-    int fontSize;
-
-    @Parameters(
-            paramLabel = "<word>",
-            defaultValue = "Hello, picocli",
-            description = "Words to be translated into ASCII art.")
-    private String[] words;
-
-    @Option(
-            names = {"-c", "--config"},
-            description = "Path to JSON config file")
-    private File configPath;
+    @CommandLine.Mixin
+    private Options options;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Application()).execute(args);
@@ -46,16 +38,27 @@ public class Application implements Runnable {
         var config = loadConfig();
         LOGGER.atInfo().addKeyValue("config", config).log("Config content");
 
-        // ... logic
+        AffineTransformation.setSeed(config.getSeed());
+
+        var generator = new Generator(config);
+        var writer = new ImageWriter(config.getOutputPath());
+
+        generator.render();
+        if (config.isGammaCorrection()) {
+            generator.correction();
+        }
+        writer.write(generator.getImage());
     }
 
     private AppConfig loadConfig() {
         // fill with cli options
-        if (configPath == null) return new AppConfig(fontSize, words);
+        if (options.configPath == null) {
+            return new AppConfig(new AppConfig.Size(options.width, options.height), options.iterationCount, options.outputPath, options.threadsCount, options.seed, options.gammaCorrection, options.gamma, options.symmetryLevel, options.functions, options.affineTransformations);
+        }
 
         // use config file if provided
         try {
-            return YAML_READER.readValue(configPath, AppConfig.class);
+            return JSON_READER.readValue(options.configPath, AppConfig.class);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
