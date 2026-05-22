ce096d81438b75d65262a9f0a4a7af9e9461e22c
Denis Skrobot <mizentui@gmail.com>
2026-05-22 15:23:22 +0300

    fix: add user id in fractal response proto

---
+++ gateway/src/main/java/com/fractalflame/gateway/mapper/FunctionMapper.java
@@ -0,0 +1,19 @@
+package com.fractalflame.gateway.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import com.fractalflame.gateway.model.Function;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface FunctionMapper {
+    Function toFunction(com.fractalflame.generator.proto.Function function);
+
+    com.fractalflame.generator.proto.Function fromFunction(Function function);
+}
