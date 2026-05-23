90051ad52bba77801547b48132e099b51cb50ae1
Denis Skrobot <mizentui@gmail.com>
2026-05-23 13:42:27 +0300

    feat: generator and gateway running via docker compose

--- generator/src/main/java/com/fractalflame/generator/repository/FractalsRepository.java
+++ generator/src/main/java/com/fractalflame/generator/repository/FractalsRepository.java
@@ -1,7 +1,10 @@
 package com.fractalflame.generator.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,6 @@ import com.fractalflame.generator.entity.Fractal;
 @Repository
 public interface FractalsRepository extends JpaRepository<Fractal, Long> {
     Page<Fractal> findAllByUserId(Long id, Pageable pageable);
+
+    List<Fractal> findAllByUserId(Long id, Sort sort);
 }
