ef6f8b64510094ad6588bee1e636ec502d79c2b3
Denis Skrobot <mizentui@gmail.com>
2026-05-26 14:59:33 +0300

    refactor: add pages metadata for fractals response and move images endpoints

--- gateway/src/main/java/com/fractalflame/gateway/configuration/GrpcConfiguration.java
+++ gateway/src/main/java/com/fractalflame/gateway/configuration/GrpcConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.grpc.client.GrpcChannelFactory;
 
 import com.fractalflame.gateway.interceptor.AuthInterceptor;
 import com.fractalflame.generator.proto.FractalsGrpc;
-import com.fractalflame.generator.proto.ImagesGrpc;
 
 @Configuration
 public class GrpcConfiguration {
@@ -20,10 +19,5 @@ public class GrpcConfiguration {
     @Bean
     FractalsGrpc.FractalsStub fractalsStub(GrpcChannelFactory channels, AuthInterceptor authInterceptor) {
         return FractalsGrpc.newStub(channels.createChannel("generator"));
-    }
-
-    @Bean
-    ImagesGrpc.ImagesBlockingStub imagesStub(GrpcChannelFactory channels) {
-        return ImagesGrpc.newBlockingStub(channels.createChannel("generator"));
     }
 }
