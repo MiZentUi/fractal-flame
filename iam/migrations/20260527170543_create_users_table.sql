f77e9bf1faa6a2c52bce99e14bdf12231c3131d7
ekuzm <ecuzmenckov@gmail.com>
2026-05-31 02:21:28 +0300

    feat: update migrations

--- iam/migrations/20260527170543_create_users_table.sql
+++ iam/migrations/20260527170543_create_users_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(60) NOT NULL,
     image VARCHAR(50)
 );
 
