2273bfbe310e7c2bfa0e0e33cbbba75c393f0904
29121970t <29121970tttt@gmail.com>
2026-06-02 13:43:29 +0300

    fix: Proper svg icons for sidebar trigger.

--- frontend/src/components/sidebar/SidebarTrigger.vue
+++ frontend/src/components/sidebar/SidebarTrigger.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import { useSidebar } from '@/components/ui/sidebar'
 import {Button } from '@/components/ui/button'
-import { computed } from 'vue';
+import { ChevronsLeftRight, ChevronsRightLeft } from 'lucide-vue-next';
 
 const { open, toggleSidebar } = useSidebar()
-const innerText = computed(() => open.value ? "⏴⏵" : "⏵⏴")
 
 
 
 </script>
 
 <template>
-  <Button class="table-cell text-2xl p-0 pb-1" @click="toggleSidebar">
-      {{ innerText }}
+  <Button class="text-2xl p-0" @click="toggleSidebar">
+        <ChevronsLeftRight v-if="open" :strokeWidth="3" :size="60"></ChevronsLeftRight>
+        <ChevronsRightLeft v-else :strokeWidth="3" :size="60"></ChevronsRightLeft>
   </Button>
 </template>
